a123cba88d55a9e0a050d8a188ab18435e2075b9
status <marco.costantini7@studio.unibo.it>
2021-11-16 17:58:27 +0100

    Create Controller Interface

--- src/it/unibo/oop/lab/mvc/Controller.java
+++ src/it/unibo/oop/lab/mvc/Controller.java
@@ -1,28 +1,41 @@
 package it.unibo.oop.lab.mvc;
 
+import java.util.List;
+
 /**
  * A controller that prints strings and has memory of the strings it printed.
  */
 public interface Controller {
+    /**
+     * Method for setting the next string to print. Null values are not acceptable.
+     * 
+     * @param stringToPrint
+     * @exception 
+     */
+    void setNextStringToPrint(String stringToPrint);
+    /**
+     * Getting the next string to print.
+     * 
+     * @return return next String to print
+     */
+    String getNextStringToPrint();
+    /**
+     * Getting the history of the printed strings.
+     * 
+     * @return List of the printed strings
+     */
+    List<String> getHistoryList();
+    /**
+     * Prints the current string. If the current string is unset,
+     * an IllegalStateException should be thrown
+     * 
+     * @exception IllegalStateException
+     */
+    void printCurrentString();
 
     /*
      * This interface must model a simple controller responsible of I/O access. It
      * considers only the standard output, and it is able to print on it.
-     * 
-     * Write the interface and implement it in a class in such a way that it
-     * includes:
-     * 
-     * 1) A method for setting the next string to print. Null values are not
-     * acceptable, and an exception should be produced
-     * 
-     * 2) A method for getting the next string to print
-     * 
-     * 3) A method for getting the history of the printed strings (in form of a List
-     * of Strings)
-     * 
-     * 4) A method that prints the current string. If the current string is unset,
-     * an IllegalStateException should be thrown
-     * 
      */
 
 }
