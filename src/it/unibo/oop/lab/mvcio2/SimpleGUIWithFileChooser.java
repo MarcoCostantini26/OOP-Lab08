81bac9482daa23c5adf559f8d85a2ad32709cd70
status <marco.costantini7@studio.unibo.it>
2021-11-16 14:03:57 +0100

    point 1 on SimpleGUIWithFileChooser done

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,19 +1,77 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUIWithFileChooser {
+    private final JFrame frame = new JFrame();
+
+    public SimpleGUIWithFileChooser() {
+        final JPanel canvas = new JPanel();
+        canvas.setLayout(new BorderLayout());
+        final JPanel canvas2 = new JPanel();
+        canvas2.setLayout(new BorderLayout());
+        canvas.add(canvas2, BorderLayout.NORTH);
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        canvas.add(save, BorderLayout.SOUTH);
+        final JTextField browseText = new JTextField();
+        canvas2.add(browseText, BorderLayout.CENTER);
+        final JButton browseButton = new JButton("Browse...");
+        canvas2.add(browseButton, BorderLayout.LINE_END);
+        frame.setContentPane(canvas);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+         * Handlers
+         */
+        save.addActionListener(new ActionListener() {
+            private Controller file = new Controller();
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    file.write(textArea.getText());
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        /*
+         *  Make the frame half the resolution of the screen.
+         */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this
+         * flag makes the OS window manager take care of the default positioning
+         * on screen. Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+        /*
+         * OK, ready to pull the frame onscreen
+         */
+        frame.setVisible(true);
+    }
 
     /*
-     * TODO: Starting from the application in mvcio:
-     * 
-     * 1) Add a JTextField and a button "Browse..." on the upper part of the
-     * graphical interface.
-     * Suggestion: use a second JPanel with a second BorderLayout, put the panel
-     * in the North of the main panel, put the text field in the center of the
-     * new panel and put the button in the line_end of the new panel.
      * 
      * 2) The JTextField should be non modifiable. And, should display the
      * current selected file.
@@ -31,5 +89,7 @@ public final class SimpleGUIWithFileChooser {
      * update the UI: in this example the UI knows when should be updated, so
      * try to keep things separated.
      */
-
+    public static void main(final String[] args) {
+        new SimpleGUIWithFileChooser();
+    }
 }
